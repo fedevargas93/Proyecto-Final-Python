29bdd1baf6f803fafb7ab92fd0a0921c8b80584f
Anaadrb <anaadrb96@gmail.com>
2023-11-13 11:18:15 +0100

    Cambios de orden y del título de bitcoin

--- Proyecto Final/proyecto_final.py
+++ Proyecto Final/proyecto_final.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np 
 import plotly.graph_objects as go
 import streamlit as st
-import custom_menu
-import indicadores
+from streamlit_option_menu import option_menu
 
 
 # Request OHLC data from Kraken API
@@ -73,6 +72,37 @@ fig.add_trace(go.Scatter(x=[ohlc.index[0], ohlc.index[-1]], y=[start, optimized_
 # Display the candlestick chart with the gradient descent line
 st.plotly_chart(fig)
 
+st.title("Final project Python Candlestick Graph")
+
+add_multiselect = st.multiselect("Chose the following available coins",
+    ("Bitcoin" , "Ethereum", "Tether", "USD Coin", "Binance Coin")
+)
+
+go_button = st.button("Go")
+
+
+if add_multiselect == "Bitcoin":
+    st.write("Display Bitcoin information or candlestick chart here")
+    # You can add more detailed information or a candlestick chart for Bitcoin
+elif add_multiselect == "Ethereum":
+    st.write("Display Ethereum information or candlestick chart here")
+    # You can add more detailed information or a candlestick chart for Ethereum
+
+elif add_multiselect == "Tether":
+    st.write("Display Tether information or candlestick chart here")
+
+elif add_multiselect == "USD Coin":
+    st.write("Display USD Coin information or candlestick chart here")
+
+elif add_multiselect == "Binance Coin":
+    st.write("Display Binance Coin information or candlestick chart here")
+
+else:
+    print("Please chose an appropiate Coin")
+
+#  Home button
+if st.button("Home"):
+    st.write("You clicked on the Home button!")
 
 
 
